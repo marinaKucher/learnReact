ad2d9244ca01c26d99a054c14e82f21e6c369be7
marina <kucher.m16@gmail.com>
2022-01-27 21:31:12 +0300

    добавила параграф для проверки работы гита

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -17,6 +16,7 @@ function App() {
         >
           Learn React
         </a>
+        <p><h1>для нового комита и пуша</h1></p>
       </header>
     </div>
   );
